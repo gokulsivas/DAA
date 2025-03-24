c689ef65552d5183da7083bfa14daf08774cf1bb
gokulsivas <gokul.rnksiva@gmail.com>
2025-03-24 20:01:05 +0530

    updated first 3 codes

--- lab.py
+++ lab.py
@@ -2,38 +2,41 @@ MAX = 20
 COLORS = 4
 color_names = ["Red", "Green", "Violet", "Blue"]
 
-graph = [[0] * MAX for _ in range(MAX)]
-color = [-1] * MAX
+graph = [[0]* MAX for _ in range(MAX)]
+colors = [-1]* MAX
 
 V = int(input("Enter the number of vertices: "))
 
-print("Enter the adjacency matrix:")
+print("Enter the adjacency matrix: ")
 graph = [list(map(int, input().split())) for _ in range(V)]
 
 def is_safe(v, c, V):
     for i in range(V):
-        if graph[v][i] == 1 and color[i] == c:
+        if graph[v][i] == 1 and colors[i] == c:
             return False
     return True
 
 def graph_coloring(v, V):
-    if v == V:
+    if (v == V):
         return True
-
+    
     for c in range(COLORS):
         if is_safe(v, c, V):
-            color[v] = c
-            if graph_coloring(v + 1, V):
+            colors[v] = c
+            if graph_coloring(v+1, V):
                 return True
-            color[v] = -1
+            colors[v] = -1
     return False
 
 def print_solution(V):
-    print("Solution with minimum colors:")
+    print("Solution using minimum colours: ")
     for i in range(V):
-        print(f"Vertex {i} -> {color_names[color[i]]}")
+        print(f"Vertex {i} -> {color_names[colors[i]]}")
 
 if graph_coloring(0, V):
     print_solution(V)
 else:
-    print("Solution is not possible with the given 4 colors for the given graph.")
+    print("Solution cannot be obtained using the 4 given colours")
+    
+
+              
